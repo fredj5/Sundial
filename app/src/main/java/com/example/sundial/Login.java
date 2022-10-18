4d04875ef43a0b029ab5a6cd89828c93bf9554ed
freddiejones <fredjjones1700@gmail.com>
2022-10-18 19:19:33 -0400

    Registration page, login page, and email verification mechanism are all complete

--- app/src/main/java/com/example/sundial/Login.java
+++ app/src/main/java/com/example/sundial/Login.java
@@ -1,19 +1,31 @@
 package com.example.sundial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-    Button createAccountBtn;
+    Button createAccountBtn, loginBtn;
+    EditText username, password;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         createAccountBtn = findViewById(R.id.createAccountButton);
         createAccountBtn.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +34,49 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), Registration.class));
             }
         });
+
+        username = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginButton);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Extract email and username data
+                if(username.getText().toString().isEmpty()) {
+                    username.setError("Please enter your email");
+                    return;
+                }
+                if(password.getText().toString().isEmpty()) {
+                    password.setError("Please enter your password");
+                }
+
+                // Validate credentials
+                firebaseAuth.signInWithEmailAndPassword(username.getText().toString(),
+                        password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        // Login success
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class ));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() != null) {
+             startActivity(new Intent(getApplicationContext(), MainActivity.class));
+             finish();
+        }
     }
 }
--- app/src/main/java/com/example/sundial/MainActivity.java
+++ app/src/main/java/com/example/sundial/MainActivity.java
@@ -5,17 +5,64 @@ import static android.provider.AlarmClock.EXTRA_MESSAGE;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    TextView username_entry;
+    TextView verifyMessage;
+    Button verifyEmailBtn;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+
+        Button logoutBtn = findViewById(R.id.logoutBtn);
+        verifyMessage = findViewById(R.id.verifyEmailMesg);
+        verifyEmailBtn = findViewById(R.id.verifyEmailBtn);
+
+        if(auth.getCurrentUser().isEmailVerified()) {
+            verifyEmailBtn.setVisibility(View.GONE);
+            verifyMessage.setVisibility(View.GONE);
+        }
+
+        verifyEmailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Send verification email
+                auth.getCurrentUser().sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(MainActivity.this, "Your verification email has been sent", Toast.LENGTH_LONG).show();
+                        Toast.makeText(MainActivity.this, "Check your spam folder if the email is not in your main inbox", Toast.LENGTH_LONG).show();
+                        verifyEmailBtn.setVisibility(View.GONE);
+                        verifyMessage.setVisibility(View.GONE);
+                    }
+                });
+            }
+        });
+
+
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+        });
 
         // Get the Intent that started this activity and extract the string
 
