408de680bb40ff31ab8b611f5bd44200a6eb6b4b
Ryan Czirr <rmczirr@syr.edu>
2022-10-19 20:30:20 -0400

    nav bar completed

--- app/src/main/java/com/example/sundial/MainActivity.java
+++ app/src/main/java/com/example/sundial/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.sundial;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.sundial.home.HomeFragment;
+import com.example.sundial.info.InfoFragment;
+import com.example.sundial.stats.StatsFragment;
+import com.example.sundial.sun.SunFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -31,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+        System.out.println("==================================================");
+        System.out.println("Curr Dest: " + navController.getCurrentDestination());
+        System.out.println("==================================================");
     }
 
 }
